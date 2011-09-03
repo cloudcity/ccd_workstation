e4f4549aa4fd892dadec4a9ad9f8bd86c46669f8
Tim Connor <timocratic@gmail.com>
2011-09-02 17:05:16 -0700

    fix rebase git config

--- recipes/git_config_global_defaults.rb
+++ recipes/git_config_global_defaults.rb
@@ -1,4 +1,9 @@
-execute "set branch autosetuprebase=true" do
-  command "git config --global branch.autosetuprebase true"
+execute "set branch autosetupmerge=false" do
+  command "git config --global branch.autosetupmerge false"
+  user WS_USER
+end
+
+execute "set branch autosetuprebase=always" do
+  command "git config --global branch.autosetuprebase always"
   user WS_USER
 end
