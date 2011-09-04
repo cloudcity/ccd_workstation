3a4833d9450070e8f715eb59f035e9a1776c730f
Tim Connor <timocratic@gmail.com>
2011-09-04 00:39:08 -0700

    some more git aliases ported from my dotfiles

--- recipes/git_config_global_defaults.rb
+++ recipes/git_config_global_defaults.rb
@@ -7,3 +7,13 @@ execute "set branch autosetuprebase=always" do
   command "git config --global branch.autosetuprebase always"
   user WS_USER
 end
+
+execute "set alias amend=commit --amend -C HEAD" do
+  command "git config --global alias.amend 'commit --amend -C HEAD'"
+  user WS_USER
+end
+
+execute "set alias conflicts opens conflicted files in editor" do
+  command "git config --global alias.conflicts '!git ls-files --unmerged | cut -c51- | sort -u | xargs $EDITOR'"
+  user WS_USER
+end
