a1e6474974f8204cacedbc52c1e33458c21e777e
Tim Connor <timocratic@gmail.com>
2011-09-20 11:05:52 -0700

    update java to use chef package provider

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,30 +1,24 @@
 version = os_x_point_version
 `pkgutil --pkgs=com.apple.pkg.JavaForMacOSX10#{version}`
 if $? != 0
-
-  remote_file "/tmp/java.dmg" do
-    source node["java_download_uri"]
-    mode "0644" 
-  end
-
-  execute "mount java dmg" do
-    command "hdid /tmp/java.dmg"
-    user WS_USER
-  end
-
-  execute "intall java" do
-    command "installer -pkg /Volumes/Java\\ for\\ Mac\\ OS\\ X\\ 10.#{version}/JavaForMacOSX10.#{version}.pkg -target /"
-  end
-
-  execute "unmount dmg" do
-    command "hdiutil detach /Volumes/Java\\ for\\ Mac\\ OS\\ X\\ 10.#{version}"
-    user WS_USER
-  end
-
-  ruby_block "test to see if java was installed" do
-    block do
-      `pkgutil --pkgs=com.apple.pkg.JavaForMacOSX10#{version}`
-      raise "Java install failed" if $? != 0
+  case version
+  when "6"
+    pivotal_workstation_package "JavaForMacOSX10.6" do
+      source "http://support.apple.com/downloads/DL1360/en_US/JavaForMacOSX10.6Update5.dmg"
+      volumes_dir "Java for Mac OS X 10.6"
+      action :install
+      type "mpkg"
+      package_id "com.apple.pkg.JavaForMacOSX106"
+      checksum "ea5515c0a8567a028a9e05f78c4e0a07ecac610018c5c32c6aeec2144663d7af"
+    end
+  when "7"
+    pivotal_workstation_package "JavaForMacOSX10.7" do
+      source "http://support.apple.com/downloads/DL1421/en_US/JavaForMacOSX10.7.dmg"
+      volumes_dir "Java for Mac OS X 10.7"
+      action :install
+      type "mpkg"
+      package_id "com.apple.pkg.JavaForMacOSX107"
+      checksum "4c32d32cfeaaf4aab539a1385c4f55a97cf09c7d396f8b43257161616bba7070"
     end
   end
 end
